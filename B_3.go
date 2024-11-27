2c3cd023aed9c9510d03fddb2fadffc9b35fd012
tahmasbi92 <atemaaaa92@gmail.com>
2024-11-27 17:09:47 +0330

    اضافه کردن فایل

---
+++ B_3.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "hello"
+	c := []byte(s)
+	// convert string to []byte type
+	c[0] = "c"
+	s2 := string(c)
+	// convert back to string type
+	fmt.Printf("%s\n", s2)
+}
