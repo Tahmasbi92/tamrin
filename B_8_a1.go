d32ccbf55469f97e4dfc2bf9068a5f0306648843
tahmasbi92 <atemaaaa92@gmail.com>
2024-12-02 16:55:48 +0330

    اضافه کردن فایل

---
+++ B_8_a1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	result := byeBread()
+
+	fmt.Println(result)
+}
+
+func byeBread() (doAdadLavash string) {
+	doAdadLavash = " 2 Adad Naan !!!"
+	return doAdadLavash
+}
