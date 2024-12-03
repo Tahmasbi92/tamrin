02d0a3feda38fe9134b9708d675fedeed2a2aa57
tahmasbi92 <atemaaaa92@gmail.com>
2024-12-03 21:05:26 +0330

    اضافه کردن فایل

--- B_13_a3.go
+++ B_13_a3.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	numbers := []int{2, 3, 5, 11, 13, 1}
-
+	// LENGTH        1  2  3   4   5  6
 	// len مخفف length یعنی طول می باشد که در این شکل 6 تا می باشد
 	fmt.Println(len(numbers))
 }
